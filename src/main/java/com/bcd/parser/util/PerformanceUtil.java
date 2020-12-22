a880f3b38ae94a780c71b9d88189989ad3469010
baichangda <471267877@qq.com>
2020-12-22 14:54:27 +0800

    c

---
+++ src/main/java/com/bcd/parser/util/PerformanceUtil.java
@@ -0,0 +1,114 @@
+package com.bcd.parser.util;
+
+import com.bcd.parser.Parser;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PerformanceUtil {
+
+    static Logger logger= LoggerFactory.getLogger(PerformanceUtil.class);
+
+    /**
+     * 测试单个线程
+     * @param data
+     * @param parser
+     * @param clazz
+     * @param num
+     * @param <T>
+     */
+    public static <T>void testSingleThreadPerformance(String data,Parser parser,Class<T> clazz,int num){
+        byte [] bytes= ByteBufUtil.decodeHexDump(data);
+        ByteBuf byteBuf= Unpooled.wrappedBuffer(bytes);
+        byteBuf.markReaderIndex();
+        byteBuf.markWriterIndex();
+        long t1=System.currentTimeMillis();
+        for(int i=1;i<=num;i++) {
+            byteBuf.resetReaderIndex();
+            byteBuf.resetWriterIndex();
+            parser.parse(clazz,byteBuf);
+        }
+        long t2=System.currentTimeMillis();
+        long diff=t2-t1;
+        logger.info("num:{} , cost time:{}ms , speed:{}/s",num,diff,(int)(num/(diff/1000d)));
+    }
+
+
+    /**
+     * 测试多线程
+     * @param data
+     * @param parser
+     * @param clazz
+     * @param poolSize
+     * @param num
+     * @param <T>
+     */
+    public static <T>void testPerformance(String data, Parser parser,Class<T> clazz,int poolSize,int num){
+        logger.info("poolSize:{}",poolSize);
+        AtomicInteger count=new AtomicInteger(0);
+
+        ExecutorService[]pools=new ExecutorService[poolSize];
+        for(int i=0;i<pools.length;i++){
+            pools[i] = Executors.newSingleThreadExecutor();
+        }
+        for (ExecutorService pool : pools) {
+            pool.execute(() -> {
+                testParse(data, parser,clazz,num, count);
+            });
+        }
+
+        ScheduledExecutorService monitor=Executors.newSingleThreadScheduledExecutor();
+        monitor.scheduleAtFixedRate(()->{
+            logger.info("speed/s:{}",count.getAndSet(0)/3);
+        },3,3,TimeUnit.SECONDS);
+
+        try {
+            for (ExecutorService pool : pools) {
+                pool.shutdown();
+            }
+            for (ExecutorService pool : pools) {
+                pool.awaitTermination(1,TimeUnit.HOURS);
+            }
+            monitor.shutdown();
+            monitor.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            logger.error("interrupted",e);
+        }
+    }
+
+    private static <T>void testParse(String data, Parser parser,Class<T> clazz, int num, AtomicInteger count){
+        byte [] bytes= ByteBufUtil.decodeHexDump(data);
+        ByteBuf byteBuf= Unpooled.wrappedBuffer(bytes);
+        byteBuf.markReaderIndex();
+        byteBuf.markWriterIndex();
+        for(int i=1;i<=num;i++) {
+            byteBuf.resetReaderIndex();
+            byteBuf.resetWriterIndex();
+            parser.parse(clazz,byteBuf);
+            count.incrementAndGet();
+        }
+    }
+
+    private static <T>void testDeParse(String data, Parser parser,Class<T> clazz, int num, AtomicInteger count){
+        byte [] bytes= ByteBufUtil.decodeHexDump(data);
+        ByteBuf byteBuf= Unpooled.wrappedBuffer(bytes);
+        T packet= parser.parse(clazz, byteBuf);
+        ByteBuf res= Unpooled.buffer(bytes.length);
+        res.markReaderIndex();
+        res.markWriterIndex();
+        for(int i=1;i<=num;i++) {
+            res.resetReaderIndex();
+            res.resetWriterIndex();
+            parser.deParse(packet,res);
+            count.incrementAndGet();
+        }
+    }
+}
--- src/test/java/com/bcd/gb32960/parser/Parser_gb32960.java
+++ src/test/java/com/bcd/gb32960/parser/Parser_gb32960.java
@@ -4,18 +4,11 @@ import com.bcd.gb32960.data.Packet;
 import com.bcd.parser.Parser;
 import com.bcd.parser.process.FieldProcessor;
 import com.bcd.parser.util.ParserUtil;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufUtil;
-import io.netty.buffer.Unpooled;
+import com.bcd.parser.util.PerformanceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Parser_gb32960 extends Parser{
 
@@ -40,90 +33,11 @@ public class Parser_gb32960 extends Parser{
         parser.init();
         String data="232303FE4C534A4132343033304853313932393639010135" +
                 "1403190F0507010203010000000469B00EE5271055020F1FFF000002010103424E1E4E2045FFFF2710050006BE437001CF306A060160FFFF0101FFFF0118FF01010E070000000000000000000801010EE527100060000160FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF09010100180EFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFED";
-        testSingleThreadPerformance(data,parser);
-//        testPerformance(data,parser);
-
+        PerformanceUtil.testSingleThreadPerformance(data,parser,Packet.class,3000000);
     }
 
-    private static void testSingleThreadPerformance(String data,Parser parser){
-        int num=3000000;
-        byte [] bytes= ByteBufUtil.decodeHexDump(data);
-        ByteBuf byteBuf= Unpooled.wrappedBuffer(bytes);
-        byteBuf.markReaderIndex();
-        byteBuf.markWriterIndex();
-        long t1=System.currentTimeMillis();
-        for(int i=1;i<=num;i++) {
-            byteBuf.resetReaderIndex();
-            byteBuf.resetWriterIndex();
-            parser.parse(Packet.class,byteBuf);
-        }
-        long t2=System.currentTimeMillis();
-        long diff=t2-t1;
-        logger.info("num:{} , cost time:{}ms , speed:{}/s",num,diff,(int)(num/(diff/1000d)));
-    }
 
-    private static void testPerformance(String data, Parser parser){
-//        int poolSize=Runtime.getRuntime().availableProcessors()+1;
-        int poolSize=1;
-        int num=10000000;
-        logger.info("poolSize:{}",poolSize);
-        AtomicInteger count=new AtomicInteger(0);
 
-        ExecutorService []pools=new ExecutorService[poolSize];
-        for(int i=0;i<pools.length;i++){
-            pools[i] = Executors.newSingleThreadExecutor();
-        }
-        for (ExecutorService pool : pools) {
-            pool.execute(() -> {
-                testParse(data, parser,num, count);
-            });
-        }
 
-        ScheduledExecutorService monitor=Executors.newSingleThreadScheduledExecutor();
-        monitor.scheduleAtFixedRate(()->{
-            logger.info("speed/s:{}",count.getAndSet(0)/3);
-        },3,3,TimeUnit.SECONDS);
-
-        try {
-            for (ExecutorService pool : pools) {
-                pool.shutdown();
-            }
-            for (ExecutorService pool : pools) {
-                pool.awaitTermination(1,TimeUnit.HOURS);
-            }
-            monitor.shutdown();
-            monitor.awaitTermination(1,TimeUnit.MINUTES);
-        } catch (InterruptedException e) {
-            logger.error("interrupted",e);
-        }
-    }
-
-    private static void testParse(String data, Parser parser, int num, AtomicInteger count){
-        byte [] bytes= ByteBufUtil.decodeHexDump(data);
-        ByteBuf byteBuf= Unpooled.wrappedBuffer(bytes);
-        byteBuf.markReaderIndex();
-        byteBuf.markWriterIndex();
-        for(int i=1;i<=num;i++) {
-            byteBuf.resetReaderIndex();
-            byteBuf.resetWriterIndex();
-            parser.parse(Packet.class,byteBuf);
-            count.incrementAndGet();
-        }
-    }
-
-    private static void testDeParse(String data, Parser parser, int num, AtomicInteger count){
-        byte [] bytes= ByteBufUtil.decodeHexDump(data);
-        ByteBuf byteBuf= Unpooled.wrappedBuffer(bytes);
-        Packet packet= parser.parse(Packet.class, byteBuf);
-        ByteBuf res= Unpooled.buffer(bytes.length);
-        res.markReaderIndex();
-        res.markWriterIndex();
-        for(int i=1;i<=num;i++) {
-            res.resetReaderIndex();
-            res.resetWriterIndex();
-            parser.deParse(packet,res);
-            count.incrementAndGet();
-        }
-    }
 
 }
