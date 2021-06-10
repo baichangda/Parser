d63f5ca1eccac4334d6ce64aef90fc17063d25b7
baichangda <471267877@qq.com>
2021-06-10 11:43:05 +0800

    c

--- src/main/java/com/bcd/parser/util/UnsafeUtil.java
+++ src/main/java/com/bcd/parser/util/UnsafeUtil.java
@@ -7,25 +7,34 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
 public class UnsafeUtil {
-    public final static Unsafe unsafe = getUnsafe();
+
+    public static Unsafe getUnsafe(){
+        return Singleton.INSTANCE;
+    }
+
+    private final static class Singleton{
+        private final static Unsafe INSTANCE = getInstance();
+        private static Unsafe getInstance() {
+            try {
+                Field field = Unsafe.class.getDeclaredField("theUnsafe");
+                field.setAccessible(true);
+                return (Unsafe) field.get(null);
+            } catch (NoSuchFieldException | SecurityException | IllegalAccessException e) {
+                throw BaseRuntimeException.getException(e);
+            }
+        }
+    }
+
 
     public static long fieldOffset(Field field){
         if(Modifier.isStatic(field.getModifiers())){
-            return unsafe.staticFieldOffset(field);
+            return getUnsafe().staticFieldOffset(field);
         }else{
-            return unsafe.objectFieldOffset(field);
+            return getUnsafe().objectFieldOffset(field);
         }
     }
 
-    public static Unsafe getUnsafe() {
-        try {
-            Field field = Unsafe.class.getDeclaredField("theUnsafe");
-            field.setAccessible(true);
-            return (Unsafe) field.get(null);
-        } catch (NoSuchFieldException | SecurityException | IllegalAccessException e) {
-            throw BaseRuntimeException.getException(e);
-        }
-    }
+
 
     public static int fieldType(Field field) {
         Class<?> fieldType = field.getType();
@@ -61,31 +70,31 @@ public class UnsafeUtil {
     public static Object getValue(Object instance, long offset, int type) {
         switch (type) {
             case 1: {
-                return unsafe.getByte(instance, offset);
+                return getUnsafe().getByte(instance, offset);
             }
             case 2: {
-                return unsafe.getShort(instance, offset);
+                return getUnsafe().getShort(instance, offset);
             }
             case 3: {
-                return unsafe.getInt(instance, offset);
+                return getUnsafe().getInt(instance, offset);
             }
             case 4: {
-                return unsafe.getLong(instance, offset);
+                return getUnsafe().getLong(instance, offset);
             }
             case 5: {
-                return unsafe.getFloat(instance, offset);
+                return getUnsafe().getFloat(instance, offset);
             }
             case 6: {
-                return unsafe.getDouble(instance, offset);
+                return getUnsafe().getDouble(instance, offset);
             }
             case 7: {
-                return unsafe.getChar(instance, offset);
+                return getUnsafe().getChar(instance, offset);
             }
             case 8: {
-                return unsafe.getBoolean(instance, offset);
+                return getUnsafe().getBoolean(instance, offset);
             }
             default: {
-                return unsafe.getObject(instance, offset);
+                return getUnsafe().getObject(instance, offset);
             }
         }
     }
@@ -101,39 +110,39 @@ public class UnsafeUtil {
     public static void setValue(Object instance, Object val, long offset, int type) {
         switch (type) {
             case 1: {
-                unsafe.putByte(instance, offset, (byte) val);
-                break;
+                getUnsafe().putByte(instance, offset, (byte) val);
+                return;
             }
             case 2: {
-                unsafe.putShort(instance, offset, (short) val);
-                break;
+                getUnsafe().putShort(instance, offset, (short) val);
+                return;
             }
             case 3: {
-                unsafe.putInt(instance, offset, (int) val);
-                break;
+                getUnsafe().putInt(instance, offset, (int) val);
+                return;
             }
             case 4: {
-                unsafe.putLong(instance, offset, (long) val);
-                break;
+                getUnsafe().putLong(instance, offset, (long) val);
+                return;
             }
             case 5: {
-                unsafe.putFloat(instance, offset, (float) val);
-                break;
+                getUnsafe().putFloat(instance, offset, (float) val);
+                return;
             }
             case 6: {
-                unsafe.putDouble(instance, offset, (double) val);
-                break;
+                getUnsafe().putDouble(instance, offset, (double) val);
+                return;
             }
             case 7: {
-                unsafe.putChar(instance, offset, (char) val);
-                break;
+                getUnsafe().putChar(instance, offset, (char) val);
+                return;
             }
             case 8: {
-                unsafe.putBoolean(instance, offset, (boolean) val);
-                break;
+                getUnsafe().putBoolean(instance, offset, (boolean) val);
+                return;
             }
             default: {
-                unsafe.putObject(instance, offset, val);
+                getUnsafe().putObject(instance, offset, val);
             }
         }
 
