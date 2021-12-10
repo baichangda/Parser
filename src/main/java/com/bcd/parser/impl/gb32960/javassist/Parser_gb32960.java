8c2de35c49649abe756bd06264fb2aa05eeb97bb
baichangda <471267877@qq.com>
2021-12-10 14:31:53 +0800

    c

--- src/main/java/com/bcd/parser/impl/gb32960/javassist/Parser_gb32960.java
+++ src/main/java/com/bcd/parser/impl/gb32960/javassist/Parser_gb32960.java
@@ -17,42 +17,46 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Parser_gb32960 extends Parser {
 
-    static Logger logger= LoggerFactory.getLogger(Parser_gb32960.class);
+    static Logger logger = LoggerFactory.getLogger(Parser_gb32960.class);
 
 
-    public static void main(String[] args){
-        Parser parser= new Parser_gb32960();
-        parser.allInOne=false;
+    public static void main(String[] args) {
+        Parser parser = new Parser_gb32960();
+        parser.allInOne = false;
         parser.init();
-        String data="232303FE4C534A4132343033304853313932393639010135" +
+        String data = "232303FE4C534A4132343033304853313932393639010135" +
                 "1403190F0507010203010000000469B00EE5271055020F1FFF000002010103424E1E4E2045FFFF2710050006BE437001CF306A060160FFFF0101FFFF0118FF01010E070000000000000000000801010EE527100060000160FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF09010100180EFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFED";
-        int threadNum=1;
-        if(args.length>=1){
-            threadNum=Integer.parseInt(args[0]);
+        int threadNum = 1;
+        if (args.length >= 1) {
+            threadNum = Integer.parseInt(args[0]);
         }
-        logger.info("param threadNum[{}]",threadNum);
-        int num=1000000000;
+        logger.info("param threadNum[{}]", threadNum);
+        int num = 100000000;
 
 //        byte [] bytes= ByteBufUtil.decodeHexDump(data);
 //        ByteBuf byteBuf= Unpooled.wrappedBuffer(bytes);
 //        parser.parse(Packet.class,byteBuf);
 
-        PerformanceUtil.testMultiThreadPerformance(data,parser, Packet.class,threadNum,num);
+//        PerformanceUtil.testMultiThreadPerformance(data,parser, Packet.class,threadNum,num);
 
 //        testMultiThreadPerformance(data,threadNum,num);
+
+        long t1 = System.currentTimeMillis();
+        parseToPacket(data, num, new AtomicInteger());
+        System.out.println(System.currentTimeMillis() - t1);
     }
 
-    private static void parseToPacket(String data,int num,AtomicInteger count){
-        byte [] bytes= ByteBufUtil.decodeHexDump(data);
-        ByteBuf byteBuf= Unpooled.wrappedBuffer(bytes);
+    private static void parseToPacket(String data, int num, AtomicInteger count) {
+        byte[] bytes = ByteBufUtil.decodeHexDump(data);
+        ByteBuf byteBuf = Unpooled.wrappedBuffer(bytes);
         byteBuf.markReaderIndex();
         byteBuf.markWriterIndex();
-        for(int i=0;i<num;i++){
+        for (int i = 0; i < num; i++) {
             byteBuf.resetReaderIndex();
             byteBuf.resetWriterIndex();
-            Packet packet=new Packet();
+            Packet packet = new Packet();
 
-            byte[] header=new byte[2];
+            byte[] header = new byte[2];
             byteBuf.readBytes(header);
             packet.setHeader(header);
 
@@ -60,24 +64,24 @@ public class Parser_gb32960 extends Parser {
 
             packet.setReplyFlag(byteBuf.readUnsignedByte());
 
-            int discardLen=0;
-            byte[] vinBytes=new byte[17];
+            int discardLen = 0;
+            byte[] vinBytes = new byte[17];
             byteBuf.readBytes(vinBytes);
-            for(int j=vinBytes.length-1;j>=0;j--){
-                if(vinBytes[j]==0){
+            for (int j = vinBytes.length - 1; j >= 0; j--) {
+                if (vinBytes[j] == 0) {
                     discardLen++;
-                }else{
+                } else {
                     break;
                 }
             }
-            String vin=new String(vinBytes,0,vinBytes.length-discardLen);
+            String vin = new String(vinBytes, 0, vinBytes.length - discardLen);
             packet.setVin(vin);
 
             packet.setEncodeWay(byteBuf.readUnsignedByte());
 
             packet.setContentLength(byteBuf.readUnsignedShort());
 
-            byte[] dataContent=new byte[packet.getContentLength()];
+            byte[] dataContent = new byte[packet.getContentLength()];
             byteBuf.readBytes(dataContent);
             packet.setDataContent(dataContent);
 
@@ -89,46 +93,44 @@ public class Parser_gb32960 extends Parser {
 
     /**
      * 手动解析 测试多线程
+     *
      * @param data
      * @param threadNum
      * @param num
      */
-    private static void testMultiThreadPerformance(String data,int threadNum,int num){
-        logger.info("threadNum:{}",threadNum);
-        AtomicInteger count=new AtomicInteger(0);
-        ExecutorService[]pools=new ExecutorService[threadNum];
-        for(int i=0;i<pools.length;i++){
+    private static void testMultiThreadPerformance(String data, int threadNum, int num) {
+        logger.info("threadNum:{}", threadNum);
+        AtomicInteger count = new AtomicInteger(0);
+        ExecutorService[] pools = new ExecutorService[threadNum];
+        for (int i = 0; i < pools.length; i++) {
             pools[i] = Executors.newSingleThreadExecutor();
         }
         for (ExecutorService pool : pools) {
             pool.execute(() -> {
-                parseToPacket(data,num,count);
+                parseToPacket(data, num, count);
             });
         }
 
-        ScheduledExecutorService monitor=Executors.newSingleThreadScheduledExecutor();
-        monitor.scheduleAtFixedRate(()->{
-            int cur=count.getAndSet(0)/3;
-            logger.info("threadNum:{} , totalSpeed/s:{} , perThreadSpeed/s:{}",threadNum,cur,cur/threadNum);
-        },3,3, TimeUnit.SECONDS);
+        ScheduledExecutorService monitor = Executors.newSingleThreadScheduledExecutor();
+        monitor.scheduleAtFixedRate(() -> {
+            int cur = count.getAndSet(0) / 3;
+            logger.info("threadNum:{} , totalSpeed/s:{} , perThreadSpeed/s:{}", threadNum, cur, cur / threadNum);
+        }, 3, 3, TimeUnit.SECONDS);
 
         try {
             for (ExecutorService pool : pools) {
                 pool.shutdown();
             }
             for (ExecutorService pool : pools) {
-                pool.awaitTermination(1,TimeUnit.HOURS);
+                pool.awaitTermination(1, TimeUnit.HOURS);
             }
             monitor.shutdown();
             monitor.awaitTermination(1, TimeUnit.MINUTES);
         } catch (InterruptedException e) {
-            logger.error("interrupted",e);
+            logger.error("interrupted", e);
         }
 
     }
-
-
-
 
 
 }
