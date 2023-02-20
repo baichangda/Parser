b789d8db48f16dd0f0c8d48d8c30bee3519cd39e
baichangda <471267877@qq.com>
2023-02-20 11:46:34 +0800

    c

--- src/main/java/com/bcd/support_parser/impl/gb32960/data/VehicleSupplementData.java
+++ src/main/java/com/bcd/support_parser/impl/gb32960/data/VehicleSupplementData.java
@@ -5,10 +5,12 @@ import com.bcd.support_parser.anno.F_date;
 import com.bcd.support_parser.anno.F_userDefine;
 import com.bcd.support_parser.impl.gb32960.processor.VehicleCommonDataFieldProcessor;
 
+import java.util.Date;
+
 public class VehicleSupplementData implements PacketData {
     //数据采集时间
     @F_date
-    public String collectTime;
+    public Date collectTime;
 
     //车辆运行通用数据
     @F_userDefine(processorClass = VehicleCommonDataFieldProcessor.class)
